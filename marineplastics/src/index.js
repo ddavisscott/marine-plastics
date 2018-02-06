f3dc76b65390de7ac945818088b1db3ab0030109
guitav <guitapardis@yahoo.com>
2018-02-05 20:05:48 -0800

    Added the landing page, image, landing page buttons, and did CSS (without SCSS)

--- marineplastics/src/index.js
+++ marineplastics/src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import styles from '/Users/guitavahdatinia/Desktop/Marine Plastics/marineplastics/src/style.css';
+// Insert the link in the #
+class Application extends React.Component {  render() {
+    return (
+      <div>
+        <button className="largebutton" onclick="window.location='https://www.google.com/';">Post</button>
+        <button className="largebutton" onclick="window.location='#';">View</button>
+      </div>
+    )
+  }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Application />, document.getElementById('content'));
