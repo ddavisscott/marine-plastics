fe2a065aa3ab040d86b71c2d13acff1ab6e3cddc
guitav <guitapardis@yahoo.com>
2018-02-20 21:11:25 -0800

    Added screen responsiveness for header.js -- dropdown yet to be added

--- client/auth.js
+++ client/auth.js
@@ -1,4 +1,4 @@
-import auth0 from 'auth0-js';
+import * as auth0 from 'auth0-js';
 
 export default class Auth {
   constructor() {
@@ -53,7 +53,7 @@ export default class Auth {
   }
 
   isAuthenticated() {
-    // Check whether the current time is past the 
+    // Check whether the current time is past the
     // Access Token's expiry time
     let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
     return new Date().getTime() < expiresAt;
--- client/modules/App/components/Header/Header.js
+++ client/modules/App/components/Header/Header.js
@@ -1,56 +1,112 @@
-import React, { PropTypes } from 'react';
-import { Link } from 'react-router';
+import React, {PropTypes} from 'react';
+import {Link} from 'react-router';
 import classNames from 'classnames';
 
 export function Header(props, context) {
-  return (
-    <nav className={
-        classNames(
-          "uk-navbar-container", 
-          "uk-navbar-transparent", 
-          "uk-margin-bottom-small"
-        )
-      }>
-      <div className="uk-navbar-left">
-        <ul className="uk-navbar-nav">
-          <li className="uk-logo">
-            <Link to="/" >
-              <h1 className="uk-text-large uk-margin-remove-bottom">
-                Marine Plastics Monitor
-              </h1>
-            </Link>
-          </li>
-          <li>
-            <Link to="/landing">Landing</Link>
-          </li>
-          <li>
-            <Link to="/test">Test Page</Link>
-          </li>
-          {
-            props.auth.isAuthenticated()
-              ? <button onClick={props.auth.logout} className="uk-button uk-button-primary">Log Out</button>
-              : <button onClick={props.auth.login} className="uk-button uk-button-primary">Log In</button>
-          }
-        </ul>
+  return (<nav className={classNames("uk-navbar-container", "uk-navbar-transparent", "uk-margin-bottom-small")}>
+
+    <div className="uk-navbar-left uk-visible@m">
+      <ul className="uk-navbar-nav">
+        <li className="uk-logo">
+          <Link to="/">
+            <h1 className="uk-text-large uk-margin-remove-bottom">
+              Marine Plastics Monitor
+            </h1>
+          </Link>
+        </li>
+        <li>
+          <Link to="/landing">Landing</Link>
+        </li>
+        <li>
+          <Link to="/test">Test Page</Link>
+        </li>
         {
-          context.router.isActive('/', true)
-            ? <a href="#" className="uk-button uk-button-primary" onClick={props.toggleAddPost}>Add Post</a>
-            : null
+          // props.auth.isAuthenticated()
+          //   ? <button onClick={props.auth.logout} className="uk-button uk-button-primary">Log Out</button>
+          //   : <button onClick={props.auth.login} className="uk-button uk-button-primary">Log In</button>
         }
-      </div>
-    </nav>
-  );
+      </ul>
+      {
+        context.router.isActive('/', true)
+          ? <a href="#" className="uk-button uk-button-primary" onClick={props.toggleAddPost}>Add Post</a>
+          : null
+      }
+    </div>
+
+    <div className="uk-navbar-left uk-visible@s uk-hidden@m">
+      <ul className="uk-navbar-nav">
+        <li className="uk-logo">
+          <Link to="/">
+            <h1 className="uk-text-small uk-margin-remove-bottom">
+              Marine Plastics Monitor
+            </h1>
+          </Link>
+        </li>
+        <li>
+          <Link to="/landing">Landing</Link>
+        </li>
+        <li>
+          <Link to="/test">Test Page</Link>
+        </li>
+        {
+          // props.auth.isAuthenticated()
+          //   ? <button onClick={props.auth.logout} className="uk-button uk-button-primary">Log Out</button>
+          //   : <button onClick={props.auth.login} className="uk-button uk-button-primary">Log In</button>
+        }
+      </ul>
+      {
+        context.router.isActive('/', true)
+          ? <a href="#" className="uk-button uk-button-primary" onClick={props.toggleAddPost}>Add Post</a>
+          : null
+      }
+    </div>
+    <div className="uk-navbar-left uk-hidden@s">
+      <ul className="uk-navbar-nav">
+        <li className="uk-logo">
+          <Link to="/">
+            <h1 className="uk-text-small uk-margin-remove-bottom">
+              Marine Plastics Monitor
+            </h1>
+          </Link>
+        </li>
+        
+          <div className="uk-nav uk-dropdown-nav">
+              <li>
+                <Link to="/landing">Landing</Link>
+              </li>
+              <li className="uk-nav-divider"></li>
+              <li>
+                <Link to="/test">Test Page</Link>
+              </li>
+
+            </div>
+
+        {
+          // props.auth.isAuthenticated()
+          //   ? <button onClick={props.auth.logout} className="uk-button uk-button-primary">Log Out</button>
+          //   : <button onClick={props.auth.login} className="uk-button uk-button-primary">Log In</button>
+        }
+      </ul>
+      {
+        context.router.isActive('/', true)
+          ? <a href="#" className="uk-button uk-button-primary" onClick={props.toggleAddPost}>Add Post</a>
+          : null
+      }
+    </div>
+
+  </nav>);
+
 }
 
 Header.contextTypes = {
-  router: React.PropTypes.object,
+  router: React.PropTypes.object
 };
 
 Header.propTypes = {
   // isAuthenticated: PropTypes.func.isRequired,
   // login: PropTypes.func.isRequired,
   // logout: PropTypes.func.isRequired,
-  toggleAddPost: PropTypes.func.isRequired,
+  toggleAddPost: PropTypes.func.isRequired
 };
 
 export default Header;
